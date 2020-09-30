b98d128129501c67c41de20ec1005575a730001f
Erwan EL <leblois.erwan@hotmail.fr>
2020-09-30 09:56:51 +0200

    [added] presentation, image, quote

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,48 @@ import Layout from "../components/layout"
 const IndexPage = () => {
   return (
     <Layout>
-      <h1 className="title">I’m in a layout!</h1>
+      <section className="hero is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-vcentered">
+              <div className="column is-half">
+                <div className="content">
+                  <h1 className="title">
+                    <span className="mate">🧉</span>
+                    <p>Bienvenue sur le Portfolio de ErwanEL></p>
+                  </h1>
+                  <p className="subtitle-mod">
+                    Intégrateur, Développeur Front-End, Javascript, GatsbyJS
+                  </p>
+                </div>
+              </div>
+              <div className="column is-half">
+                <figure className="image is-256x256">
+                  <img src="gif.gif" alt="" />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="has-text-centered">
+        <div className="container has-text-centered">
+          <div className="columns">
+            <div className="column is-half" style={{ margin: "auto" }}>
+              <div className="content">
+                <p className="quote subtitle-mod">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  harum minima voluptatem veritatis exercitationem saepe
+                  incidunt. Sit quis earum ipsam!
+                </p>
+              </div>
+              <button className="button is-success">
+                Voir le profil Upwork
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
