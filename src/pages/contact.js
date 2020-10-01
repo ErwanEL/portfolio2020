58dc77b89ed320ea7bd995bba60f3fc2a62c985e
Erwan EL <leblois.erwan@hotmail.fr>
2020-10-01 19:17:03 +0200

    [added] netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -23,47 +23,53 @@ const ContactPage = () => {
                 </div>
               </div>
               <div className="column is-half">
-                <div class="field">
-                  <label class="label">Name</label>
-                  <div class="control">
-                    <input class="input" type="text" placeholder="Text input" />
+                <form name="contact" method="POST" data-netlify="true">
+                  <div class="field">
+                    <label class="label">Name</label>
+                    <div class="control">
+                      <input
+                        name="name"
+                        class="input"
+                        type="text"
+                        placeholder="Text input"
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div class="field">
-                  <label class="label">Email</label>
-                  <div class="control has-icons-left has-icons-right">
-                    <input
-                      class="input is-danger"
-                      type="email"
-                      placeholder="Email input"
-                      value="hello@"
-                    />
-                    <span class="icon is-small is-left">
-                      <i class="fas fa-envelope"></i>
-                    </span>
-                    <span class="icon is-small is-right">
-                      <i class="fas fa-exclamation-triangle"></i>
-                    </span>
+                  <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control has-icons-left has-icons-right">
+                      <input
+                        name="email"
+                        class="input "
+                        type="email"
+                        placeholder="Email input"
+                      />
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                      </span>
+                    </div>
                   </div>
-                  <p class="help is-danger">This email is invalid</p>
-                </div>
 
-                <div class="field">
-                  <label class="label">Message</label>
-                  <div class="control">
-                    <textarea
-                      class="textarea"
-                      placeholder="Textarea"
-                    ></textarea>
+                  <div class="field">
+                    <label class="label">Message</label>
+                    <div class="control">
+                      <textarea
+                        name="message"
+                        class="textarea"
+                        placeholder="Textarea"
+                      ></textarea>
+                    </div>
                   </div>
-                </div>
 
-                <div class="field is-grouped">
-                  <div class="control">
-                    <button class="button is-link submit">Submit</button>
+                  <div class="field is-grouped">
+                    <div class="control">
+                      <button type="submit" class="button is-link submit">
+                        Submit
+                      </button>
+                    </div>
                   </div>
-                </div>
+                </form>
               </div>
             </div>
           </div>
