0c2b2bcf9900f3c8b98cb7956f5690156ac8dbbb
ErwanEL <leblois.erwan@hotmail.fr>
2020-12-24 13:16:32 +0100

    [modif] test page proto

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,45 +7,31 @@ import UpworkSvg from "../components/svg/upworksvg"
 import { cmsImageFinder } from "../components/utils"
 import YAMLData from "../../site/content/pages/test/test-fr.yml"
 
-const FreelancePage = ({ data }) => {
-  const { markdownRemark } = data
-  const { frontmatter } = markdownRemark
+const FreelancePage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      folderImages: allFile(
+        filter: { relativeDirectory: { glob: "content/pages/test/images" } }
+      ) {
+        edges {
+          node {
+            relativePath
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
   const { folderImages } = data
   const images = folderImages.edges
 
-  console.log(cmsImageFinder(images, "section0Image", YAMLData))
-
-  // const cmsImageFinder = img => {g
-  //   return images.find(
-  //     image => getPath(image.node.relativePath) === getPath(YAMLData[img])
-  //   ).node.childImageSharp.fluid  // const getPath = path => {
-  //   return path.substr(path.lastIndexOf("/") + 1)
-  // }
-  console.log(images)
-  // }
-
-  // console.log(found("section0Image"))
-  // const testFunction = (arr, img) => {
-  //   return arr.filter(
-  //     image =>
-  //       getPath(image["node"]["relativePath"]) === getPath(YAMLData["ima"])
-  //   )[0].node.childImageSharp.fluid
-  // }
-
-  // console.log(testFunction(images, section0Image))
-
-  const section0Image = cmsImageFinder(images, "section0Image", YAMLData)
-
-  const section1Image = cmsImageFinder(images, "section1Image", YAMLData)
-
-  // const section1Image = images.filter(
-  //   image =>
-  //     getPath(image.node.relativePath) === getPath(YAMLData.section1Image)
-  // )[0].node.childImageSharp.fluid
-
   return (
     <Layout>
-      <SEO title={frontmatter.seo} />
+      <SEO title={YAMLData.seo} />
       <section className="hero is-medium">
         <div className="hero-body">
           <div className="container">
@@ -59,13 +45,13 @@ const FreelancePage = ({ data }) => {
                   <hr style={{ background: "#6FDA44" }} className="mini" />
                   <UpworkSvg height={40}></UpworkSvg>
                   <p className="subtitle-mod">
-                    {frontmatter.title1}{" "}
+                    {YAMLData.section0Title}{" "}
                     <a
                       className="upwork-link"
-                      href="http://etmg-world.com"
+                      href={YAMLData.section0Link}
                       target="_blank"
                     >
-                      {frontmatter.upwork}
+                      {YAMLData.section0LinkTitle}
                     </a>
                   </p>
                 </div>
@@ -73,7 +59,7 @@ const FreelancePage = ({ data }) => {
               <div className="column is-half">
                 <Img
                   className="free image"
-                  fluid={section0Image}
+                  fluid={cmsImageFinder(images, "section0Image", YAMLData)}
                   alt="Upwork contracts"
                 />
               </div>
@@ -92,20 +78,24 @@ const FreelancePage = ({ data }) => {
           <div className="container">
             <div className="columns is-vcentered">
               <div className="column is-half">
-                <Img className="free image" fluid={section1Image} alt="Etmg" />
+                <Img
+                  className="free image"
+                  fluid={cmsImageFinder(images, "section1Image", YAMLData)}
+                  alt="Etmg"
+                />
               </div>
 
               <div className="column is-half has-text-left">
                 <div className="content">
                   <h1 className="title">Etmg</h1>
                   <p className="subtitle-mod">
-                    {frontmatter.title2}{" "}
+                    {YAMLData.section1Title}{" "}
                     <a
                       className="etmg-link"
-                      href="http://etmg-world.com"
+                      href={YAMLData.section1Link}
                       target="_blank"
                     >
-                      Etmg-World
+                      {YAMLData.section1LinkTitle}
                     </a>
                   </p>
                 </div>
@@ -117,34 +107,5 @@ const FreelancePage = ({ data }) => {
     </Layout>
   )
 }
-
-export const query = graphql`
-  {
-    markdownRemark(frontmatter: { slug: { eq: "/freelance" } }) {
-      html
-      frontmatter {
-        slug
-        seo
-        title1
-        upwork
-        title2
-      }
-    }
-    folderImages: allFile(
-      filter: { relativeDirectory: { glob: "content/pages/test/images" } }
-    ) {
-      edges {
-        node {
-          relativePath
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
 
 export default FreelancePage
