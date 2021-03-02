545af64bd6d8030aeb4b9d788eb62f52dc204819
ErwanEL <leblois.erwan@hotmail.fr>
2021-03-02 10:16:18 +0100

    added success page

---
+++ src/pages/success.js
@@ -0,0 +1,46 @@
+import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
+
+import Layout from "../components/layout"
+import React from "react"
+import SEO from "../components/seo"
+import { graphql } from "gatsby"
+
+const SuccessPage = ({ intl }) => {
+  return (
+    <Layout>
+      <SEO
+        lang={intl.locale}
+        title={intl.formatMessage({ id: "contact.section.title" })}
+      />
+      <section className="hero is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-vcentered">
+              <div className="column is-half">
+                {/* {html} */}
+                <h1 className="title">
+                  <span className="mate">✉️</span>
+                  <FormattedMessage id="contact.section.title" />
+                </h1>
+                <div className="content">
+                  <hr className="mini" />
+                  <p className="subtitle-mod">
+                    <FormattedMessage id="contact.section.description" />
+                  </p>
+                </div>
+              </div>
+              <div className="column is-half">
+                <div className="notification has-text-centered has-text-white">
+                  <FormattedMessage id="contact.success" />{" "}
+                  <i class="fas fa-check-square"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default injectIntl(SuccessPage)
